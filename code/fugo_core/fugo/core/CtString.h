9cab60a6680dde1c15d5394862bebbf015680ccc
Sergey Kovalevich <inndie@gmail.com>
2026-03-26 21:22:37 +0300

    refactoring

--- code/fugo_core/fugo/core/CtString.h
+++ code/fugo_core/fugo/core/CtString.h
@@ -3,7 +3,6 @@
 
 #pragma once
 
-#include <array>
 #include <string_view>
 
 namespace fugo {
@@ -12,7 +11,7 @@ namespace core {
 /// Compile-time string
 template <std::size_t N>
 struct CtString {
-    std::array<char, N> data{};
+    char data[N] = {0};
 
     /// Constructor: construct empty string
     consteval CtString() = default;
@@ -24,25 +23,20 @@ struct CtString {
         }
     }
 
-    /// Constructor: construct from str and size
-    consteval explicit CtString(char const* str, std::size_t size) noexcept {
-        for (std::size_t i = 0; i < size; ++i) {
-            data[i] = str[i];
-        }
-    }
-
-    /// Constructor: from std::string_view
-    constexpr explicit CtString(std::string_view str) noexcept : CtString(str.data(), str.size()) {}
-
     /// Implied convert to std::string_view
     [[nodiscard]] constexpr operator std::string_view() const noexcept {
-        return std::string_view(data.data(), data.size());
+        return std::string_view{data, N - 1};
+    }
+
+    /// Return c-string
+    [[nodiscard]] constexpr auto c_str() const noexcept -> char const* {
+        return data;
     }
 };
 
 /// CTAD
 template <std::size_t N>
-CtString(char const (&)[N]) -> CtString<N - 1>;
+CtString(char const (&)[N]) -> CtString<N>;
 
 } // namespace core
 
